df9591b2afe1c97ee48f48c926707cb93f06269e
F Y Tech <fytech@Hibos-MacBook-Air.local>
2024-05-25 20:48:46 +0100

    Added db creation with new expo-sqlite syntax

--- App.js
+++ App.js
@@ -1,61 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, TextInput, View, SafeAreaView} from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as SQLite from 'expo-sqlite';
 
 export default function App() {
+  const [isDisplayPageShown, setIsDisplayPageShown] = useState(false);
+  const [result, setResult] = useState([]);
 
-  const [showDisplayPage, setShowDisplayPage] = useState(false)
+  useEffect(() => {
+    const initDb = async () => {
+      const db = await SQLite.openDatabaseAsync('test_db');
+
+      await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+        INSERT INTO test (value, intValue) VALUES ('test1', 123);
+        INSERT INTO test (value, intValue) VALUES ('test2', 456);
+        INSERT INTO test (value, intValue) VALUES ('test3', 789);
+      `);
+
+      const res = await db.getAllAsync('SELECT * FROM test');
+      setResult(res);
+    };
+
+    initDb();
+  }, []);
 
   return (
     <View>
-
-    <StatusBar hidden />
-
-      <NavigationBar 
-        setShowDisplayPage={setShowDisplayPage}
-        />
+      <StatusBar hidden />
+      <NavigationBar setIsDisplayPageShown={setIsDisplayPageShown} />
 
       <View>
-        {!showDisplayPage && <InputPage />}
-        {showDisplayPage && <DisplayPage />}        
+        {!isDisplayPageShown && <InputPage result={result} />}
+        {isDisplayPageShown && <DisplayPage />}
       </View>
+    </View>
+  );
+}
 
-
-    </View>      
-
+const InputPage = ({ result }) => {
+  return (
+    <>
+      <Text>Input Page</Text>
+      <Text>{JSON.stringify(result)}</Text>
+    </>
   );
 };
 
-const InputPage = () => {
-  return (
-    <Text>Input Page</Text>
-  )
-}
-
 const DisplayPage = () => {
-  return (
-    <Text>Display Page</Text>
-  )
-}
+  return <Text>Display Page</Text>;
+};
 
-const NavigationBar = ({ setShowDisplayPage }) => {
-
-  const handlePressInputPage = () => {
-    setShowDisplayPage(false)
-  }
-  const handlePressDisplayPage = () => {
-    setShowDisplayPage(true)
-  }
+const NavigationBar = ({ setIsDisplayPageShown }) => {
+  const handlePressInputPage = () => setIsDisplayPageShown(false);
+  const handlePressDisplayPage = () => setIsDisplayPageShown(true);
 
   return (
-      <View>
-        <Pressable onPress={handlePressInputPage}>
-          <Text>Input Page</Text>
-        </Pressable>
-        <Pressable onPress={handlePressDisplayPage}>
-          <Text>Display Page</Text>
-        </Pressable>
-      </View>
-  )
-}
+    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+      <Pressable onPress={handlePressInputPage}>
+        <Text>Input Page</Text>
+      </Pressable>
+      <Pressable onPress={handlePressDisplayPage}>
+        <Text>Display Page</Text>
+      </Pressable>
+    </View>
+  );
+};
